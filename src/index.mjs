c3f5c3bfcf674627a975c8589ca2b3f365cd9a36
Luis Lobo <luis.lobo@openclick.ai>
2025-07-07 16:31:33 -0300

    ordenado imports conforme uso

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,10 @@
-import rehypeSanitize from "rehype-sanitize"
-import rehypeStringify from "rehype-stringify"
-import remarkParse from "remark-parse"
-import remarkRehype from "remark-rehype"
 import { unified } from "unified"
+import remarkParse from "remark-parse"
 import remarkFrontmatter from "remark-frontmatter"
 import remarkGfm from "remark-gfm"
+import remarkRehype from "remark-rehype"
+import rehypeSanitize from "rehype-sanitize"
+import rehypeStringify from "rehype-stringify"
 
 const remarkProcessor = unified()
     .use(remarkParse)
